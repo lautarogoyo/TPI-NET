65daacd8dd3cc65f1dfa966293c94dde0505820c
geronimo <geronimonegri@gmail.com>
2025-09-24 22:35:11 -0300

    Avance con WindowsForm de curso

---
+++ Gestion academica/WindowsForm/CursoDetalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm
+{
+    public partial class CursoDetalle : Form
+    {
+        public CursoDetalle()
+        {
+            InitializeComponent();
+        }
+
+        private void CursoDetalle_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
